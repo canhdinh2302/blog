e6be75029b908116b8afb4087a7ef204756f1862
ba nguyen <nguyenhuuba0797@gmail.com>
2021-10-09 20:25:20 +0700

    handle vote

--- components/ratting-stars/index.tsx
+++ components/ratting-stars/index.tsx
@@ -1,27 +1,40 @@
 import { memo } from 'react'
 import ReactStars from 'react-rating-stars-component'
+import LoginForm from '@modules/loginForm'
+import useModal from '@hooks/useModal'
 
 const RattingStars = (props) => {
   const { rattingData } = props
+  const { openModal, Modal } = useModal({
+    title: 'Đăng Nhập',
+  })
   const ratingChanged = async (point) => {
     try {
       const { voteStar } = await import('../../api/blogAPI')
-      const res = await voteStar('lead-marketing-assistant', point)
-      console.log(res)
+      await voteStar('lead-marketing-assistant', point)
     } catch (error) {
-      console.log(error)
+      if (error.response !== undefined && error.response.data.statusCode === 401) {
+        openModal()
+      } else {
+        alert('Lỗi kết nỗi')
+      }
     }
   }
 
   return (
-    <ReactStars
-      value={rattingData.value}
-      count={5}
-      size={rattingData.size}
-      edit={rattingData.edit}
-      activeColor="#ffd700"
-      onChange={ratingChanged}
-    />
+    <>
+      <ReactStars
+        value={rattingData.value}
+        count={5}
+        size={rattingData.size}
+        edit={rattingData.edit}
+        activeColor="#ffd700"
+        onChange={ratingChanged}
+      />
+      <Modal>
+        <LoginForm />
+      </Modal>
+    </>
   )
 }
 RattingStars.defaultProps = {
